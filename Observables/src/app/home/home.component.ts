76c176d59645bca028dcba6d9aacc6954c8d6d91
Harsh Singla <harshsingla@Harshs-MacBook-Pro.local>
2021-03-13 19:38:34 -0500

    Created Custom Observable

--- Observables/src/app/home/home.component.ts
+++ Observables/src/app/home/home.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private firstSubscriptionObject: Subscription;
   constructor() { }
 
   ngOnInit() {
+    // this.firstSubscriptionObject = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // })
+       const customObservable = Observable.create(observer => {
+           let count = 0;
+           setInterval(() =>{
+             observer.next(count);
+             count++;
+           },1000);
+       });
+       this.firstSubscriptionObject = customObservable.subscribe(data =>{
+         console.log(data);
+       })
   }
+  ngOnDestroy(){
+    this.firstSubscriptionObject.unsubscribe();
+    }
 
 }
